04524f7461a6e2f83b3f51ef48b9249cd3532bd6
Rainbow <pbalourd@yahoo.com>
2023-12-14 22:11:30 +0200

    Day05 Part 2 added

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -16,17 +16,16 @@ fun main(args: Array<String>) {
                 .drop(1)
                 .map {
                     val nums = it.split(" ").map { n -> n.toLong() }
-                    Pair(nums[1]..< nums[1] + nums[2], nums[0])
+                    Pair(nums[1]..< nums[1] + nums[2], nums[0] - nums[1])
                 }
         }
-
     var minLocation = Long.MAX_VALUE
     for (s in seeds) {
         var value = s
         for (map in maps) {
             for (r in map) {
                 if (value in r.first) {
-                    value = r.second + value - r.first.first
+                    value += r.second
                     break
                 }
             }
@@ -36,4 +35,67 @@ fun main(args: Array<String>) {
 
     println(minLocation)
     // Solution 510109797
+
+    val inputRanges = mutableListOf<LongRange>()
+    for (i in 0..seeds.lastIndex step 2) {
+        inputRanges.add(seeds[i]..< seeds[i] + seeds[i + 1])
+    }
+
+    minLocation = Long.MAX_VALUE
+    for (inputRange in inputRanges) {
+        val rangesToCheck = mutableListOf<LongRange>()
+        rangesToCheck.add(inputRange)
+        for (map in maps) {
+            val tempChecked = mutableListOf<LongRange>()
+            for (m in map) {
+                val tempToCheck = mutableListOf<LongRange>()
+                val mapRange = m.first
+                val mapValue = m.second
+
+                for (t in rangesToCheck) {
+                    val res = rangeIntersection(t, mapRange, mapValue)
+                    tempToCheck += res.first
+                    tempChecked += res.second
+                }
+                rangesToCheck.clear()
+                for (r in tempToCheck) rangesToCheck.add(r)
+            }
+            for (r in tempChecked) rangesToCheck.add(r)
+        }
+        rangesToCheck.forEach { if (it.first < minLocation) minLocation = it.first }
+    }
+    println(minLocation)
+    // Solution 9622622
+}
+
+fun rangeIntersection(inputRange: LongRange, mapRange: LongRange, mapValue: Long): Pair<List<LongRange>, List<LongRange>> {
+    val noChangedRanges = mutableListOf<LongRange>()
+    val changedRanges = mutableListOf<LongRange>()
+
+    // inputRange within mapRange
+    if (inputRange.first >= mapRange.first && inputRange.last <= mapRange.last) {
+        changedRanges.add(inputRange.first + mapValue..inputRange.last + mapValue)
+    }
+    // inputRange upper subset of mapRange
+    else if (inputRange.first >= mapRange.first && inputRange.first <= mapRange.last) {
+        changedRanges.add(inputRange.first + mapValue..mapRange.last + mapValue)
+        noChangedRanges.add(mapRange.last + 1..inputRange.last)
+    }
+    // inputRange lower subset of mapRange
+    else if (inputRange.last <= mapRange.last && inputRange.last >= mapRange.first) {
+        changedRanges.add(mapRange.first + mapValue..inputRange.last + mapValue)
+        noChangedRanges.add(inputRange.first..<mapRange.first)
+    }
+    // mapRange within inputRange
+    else if (inputRange.first < mapRange.first && inputRange.last > mapRange.last) {
+        changedRanges.add(mapRange.first + mapValue..mapRange.last + mapValue)
+        noChangedRanges.add(inputRange.first..<mapRange.first)
+        noChangedRanges.add(mapRange.last + 1..inputRange.last)
+    }
+    // No intersection
+    else {
+        noChangedRanges.add(inputRange)
+    }
+
+    return Pair(noChangedRanges, changedRanges)
 }
